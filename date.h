0d21cc2205fdbcc5b782021861e8f17fd057a70b
bsilawko <mrufkazkrem@protonmail.com>
2024-05-30 17:57:02 +0200

    Upload date.h

---
+++ date.h
@@ -0,0 +1,20 @@
+#pragma once
+#include <iostream>
+class Date {
+   private:
+    unsigned int day;
+    unsigned int month;
+    unsigned int year;
+    bool isValidDate(unsigned int day, unsigned int month,
+                     unsigned int year) const;
+    unsigned int daysInMonth(unsigned int month, unsigned int year) const;
+    bool isLeapYear(unsigned int year) const;
+
+   public:
+    Date(unsigned int day, unsigned int month, unsigned int year);
+    unsigned int getDay() const;
+    unsigned int getMonth() const;
+    unsigned int getYear() const;
+    bool isEqualOrGreater(const Date& other) const;
+    friend std::ostream& operator<<(std::ostream& os, Date const& d);
+};
