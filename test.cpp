e64b63e43fe91bcf07f80f414393d494088029f2
Tomasz <tom.p.smolen@gmail.com>
2024-05-30 17:43:26 +0200

    halo

---
+++ test.cpp
@@ -0,0 +1,49 @@
+#include "film.h"
+#include <fstream>
+#include <sstream>
+#include <iostream>
+#include "csv.h"
+
+int main()
+{
+    std::vector<Film> list;
+    io::CSVReader<10, io::trim_chars<' '>, io::no_quote_escape<';'>> in("../movies2.csv");
+    in.read_header(io::ignore_missing_column, "ID", "Title", "Genres", "Year", "Runtime", "Description", "Cast", "Director", "Writers", "Producers");
+    unsigned int ID;
+    std::string Title;
+    std::string Genres;
+    unsigned int Year;
+    unsigned int Runtime;
+    std::string Description;
+    std::string Cast;
+    std::string Director;
+    std::string Writers;
+    std::string Producers;
+    while (in.read_row(ID, Title, Genres, Year, Runtime, Description, Cast, Director, Writers, Producers))
+    {
+        Film f(ID, Title, Year, Genres, Cast, Description, {}, Runtime, Writers, Producers, Director);
+        list.push_back(f);
+    }
+    // std::ofstream out("../movies2.csv");
+    // out << "ID;Title;Genres;Year;Runtime;Description;Cast;Director;Writers;Producers\n";
+    // for (auto film : list)
+    // {
+    //     out << film << '\n';
+    // }
+
+    // std::ifstream input("../movies.csv");
+    // if (!input.is_open())
+    // {
+    //     std::cerr << "Unable to open file movies.csv";
+    // }
+    // std::string row;
+    // getline(input, row, '\n');
+    // while (getline(input, row, '\n'))
+    // {
+    //     if (row == "")
+    //         break;
+    //     Film f;
+    //     std::istringstream(row) >> f;
+    //     list.push_back(f);
+    return 0;
+}
