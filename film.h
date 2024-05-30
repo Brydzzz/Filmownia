a15892551318a755e93c330b72efe93e57e10707
Tomasz <tom.p.smolen@gmail.com>
2024-05-30 17:39:54 +0200

    initial commit

---
+++ film.h
@@ -0,0 +1,71 @@
+#pragma once
+#include <string>
+#include <vector>
+#include "review.h"
+#include <sstream>
+#include <iomanip>
+#include <utility>
+#include <map>
+
+enum class film_genre
+{
+    Action,
+    Adventure,
+    Comedy,
+    Drama,
+    Fantasy,
+    Horror,
+    Mystery,
+    Romance,
+    SciFi,
+    Thriller,
+    Western,
+    Unknown
+};
+
+class Review;
+class Film
+{
+    unsigned int id;
+    std::string title;
+    unsigned int prod_year;
+    std::vector<film_genre> genre;
+    std::map<std::string, std::string> cast;
+    std::string desc;
+    std::vector<Review> reviews;
+    std::string director;
+    std::map<std::string, std::string> writers;
+    std::map<std::string, std::string> producers;
+    unsigned int time;
+    std::map<std::string, std::string> parsePeople(std::string &people);
+    std::vector<film_genre> parseGenres(std::string &content);
+
+public:
+    Film(unsigned int id, std::string title, unsigned int prod_year, std::vector<film_genre> genre, std::map<std::string, std::string> cast, std::string desc, std::vector<Review> reviews, unsigned int time, std::map<std::string, std::string> writers, std::map<std::string, std::string> producers, std::string director)
+        : id(id), title(title), prod_year(prod_year), genre(genre), cast(cast), desc(desc), reviews(reviews), time(time), writers(writers), producers(producers), director(director)
+    {
+    }
+
+    Film(unsigned int id, std::string title, unsigned int prod_year, std::string genre, std::string cast, std::string desc, std::vector<Review> reviews, unsigned int time, std::string writers, std::string producers, std::string director)
+        : id(id), title(title), prod_year(prod_year), genre(parseGenres(genre)), cast(parsePeople(cast)), desc(desc), reviews(reviews), time(time), writers(parsePeople(writers)), producers(parsePeople(producers)), director(director)
+    {
+    }
+    Film(){};
+    void addReview(Review review);
+    const unsigned int &getID() const;
+    const std::vector<Review> &getReviews() const;
+    const std::string &getTitle() const;
+    const unsigned int &getYear() const;
+    const std::map<std::string, std::string> &getCast() const;
+    const std::string &getDesc() const;
+    const std::string &getDir() const;
+    const unsigned int &getTime() const;
+    const std::vector<film_genre> &getGenre() const;
+    unsigned int getRating() const;
+    friend bool operator==(const Film &lfilm, const Film &rfilm);
+    friend std::ostream &operator<<(std::ostream &os, Film &fm);
+    friend std::istream &operator>>(std::istream &is, Film &fm);
+    std::ostream &write(std::ostream &os);
+    const std::map<std::string, std::string> &getWriters() const;
+    const std::map<std::string, std::string> &getProducers() const;
+};
