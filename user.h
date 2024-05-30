21dd537593264d16fb9eed9c46943a05b208cdcb
klukiewi <kingal@itbs.pl>
2024-05-30 17:57:07 +0200

    Upload New File

---
+++ user.h
@@ -0,0 +1,24 @@
+#pragma once
+#include <ctype.h>
+#include <stdio.h>
+
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <vector>
+
+// tymczasowo zakładamy ze pierwsze dane w pliku to admin
+class Role;
+class User {
+    std::string login = "guest";
+    Role *role;
+
+   public:
+    User() = default;
+    const std::string &getLogin() const;
+    void setLogin(std::string const &new_login);
+    const Role *const &getRole() const;
+    void setRole(Role *new_role);
+    void log_in(std::string fname = "../passwords.txt");
+    void log_out();
+};
