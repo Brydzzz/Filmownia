64af1ec31e1401d649cc956abe8da8cbb482f242
klukiewi <kingal@itbs.pl>
2024-05-30 17:58:07 +0200

    Upload role

---
+++ role.cpp
@@ -0,0 +1,12 @@
+#include "role.h"
+
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <vector>
+
+User* Role::getUser() { return user; }
+// TO DO
+//  void Role::browse() {
+
+// }
