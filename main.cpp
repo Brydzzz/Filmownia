62446060400aa4be0ab6c72c49bd629d6693710e
Tomasz <tom.p.smolen@gmail.com>
2024-05-31 16:19:18 +0200

    huge progress actually

--- main.cpp
+++ main.cpp
@@ -1,6 +1,9 @@
+#pragma once
 #include <memory>
 #include "page.h"
 #include "user.h"
+#include "csv.h"
+#include "film.h"
 class InputSimulator
 {
     std::istringstream input;
@@ -12,6 +15,29 @@ public:
     }
 };
 
+std::vector<Film> loadFilms()
+{
+    std::vector<Film> list;
+    io::CSVReader<10, io::trim_chars<' '>, io::no_quote_escape<';'>> in("../movies.csv");
+    in.read_header(io::ignore_missing_column, "ID", "Title", "Genres", "Year", "Runtime", "Description", "Cast", "Director", "Writers", "Producers");
+    unsigned int ID;
+    std::string Title;
+    std::string Genres;
+    unsigned int Year;
+    unsigned int Runtime;
+    std::string Description;
+    std::string Cast;
+    std::string Director;
+    std::string Writers;
+    std::string Producers;
+    while (in.read_row(ID, Title, Genres, Year, Runtime, Description, Cast, Director, Writers, Producers))
+    {
+        Film f(ID, Title, Year, Genres, Cast, Description, {}, Runtime, Writers, Producers, Director);
+        list.push_back(f);
+    }
+    return list;
+}
+std::vector<Film> flist = loadFilms();
 int main()
 {
     User user;
@@ -20,9 +46,12 @@ int main()
     pg_ptr->print();
     int act = 0;
     act = pg_ptr->nextAction();
-
     while (act != 0)
     {
+        pg_ptr = std::move(pg_ptr->doAction(act));
+        pg_ptr->print();
+        pg_ptr->showOptions();
+        act = pg_ptr->nextAction();
     };
     return 0;
 }
--- page.h
+++ page.h
@@ -1,8 +1,12 @@
 #pragma once
+#include <memory>
+#include <limits>
 #include "film.h"
 #include "iostream"
 #include "cppio.hpp"
 #include "algorithm"
+// #include "main.cpp"
+extern std::vector<Film> flist;
 class Page
 {
     std::vector<std::string> options;
@@ -17,6 +21,7 @@ public:
     };
     virtual void print() = 0;
     virtual int nextAction() = 0;
+    virtual std::unique_ptr<Page> doAction(int act) = 0;
 };
 
 class FilmPage : public Page
@@ -41,8 +46,106 @@ public:
             return 3;
         }
     }
+    std::unique_ptr<Page> doAction(int act) override
+    {
+    }
+    void showOptions()
+    {
+        std::cout << "Avaiable Options: \n";
+        for (auto opt : options)
+        {
+            std::cout << opt << std::endl;
+        }
+    }
 };
+class BrowsePage : public Page
+{
+    std::string msg = "Browse Movies: ";
+    std::vector<std::string> options = {"Exit", "Browse"};
 
+public:
+    void print() override
+    {
+        std::cout << msg << std::endl;
+    }
+    std::vector<Film> movieSearch(const std::string &title)
+    {
+        auto it = flist.begin();
+        std::vector<Film> result = {};
+        while (it != flist.end())
+        {
+            it = std::find_if(it, flist.end(), [&](const Film &film)
+                              { return film.getTitle().find(title) != std::string::npos; });
+            if (it == flist.end())
+            {
+                break;
+            }
+            Film f = *it;
+            ++it;
+            result.push_back(f);
+            // std::cout << f.getTitle() << std::endl;
+        }
+        return result;
+    }
+    std::unique_ptr<Page> doAction(int act) override
+    {
+        if (act == 3)
+        {
+            std::string title;
+            std::cout << "Searched movie: " << std::endl;
+            std::cin.clear();
+            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+            std::getline(std::cin, title);
+            // std::cin >> title1;
+            // cppIO::input("Searched movie: ", title1, title2, title3, title4);
+            std::vector<Film> found = movieSearch(title);
+            if (found.size() != 0)
+            {
+                std::cout << "Found movies: " << std::endl;
+                int i = 1;
+                for (auto f : found)
+                {
+                    if (i <= 10)
+                    {
+                        std::cout << i << '.' << f.getTitle() << std::endl;
+                        ++i;
+                    }
+                }
+            }
+            else
+            {
+                std::cout << "Movie not found\n";
+            }
+            std::unique_ptr<BrowsePage> ptr = std::make_unique<BrowsePage>();
+            return ptr;
+        }
+    }
+    int nextAction() override
+    {
+        std::string action;
+        while (std::find(options.begin(), options.end(), action) == options.end())
+        {
+            cppIO::input("Enter desired action: ", action);
+        }
+        if (action == "Exit")
+        {
+            return 0;
+        }
+        else if (action == "Browse")
+        {
+            return 3;
+        }
+        return 5;
+    }
+    void showOptions()
+    {
+        std::cout << "Avaiable Options: \n";
+        for (auto opt : options)
+        {
+            std::cout << opt << std::endl;
+        }
+    }
+};
 class StartPage : public Page
 {
     std::vector<std::string> options = {"Browse",
@@ -67,8 +170,25 @@ public:
         }
         return 0;
     }
+    std::unique_ptr<Page> doAction(int act) override
+    {
+        if (act == 4)
+        {
+            std::unique_ptr<BrowsePage> ptr;
+            ptr = std::make_unique<BrowsePage>();
+            return ptr;
+        }
+    }
     void print() override
     {
         std::cout << msg << std::endl;
+    }
+    void showOptions()
+    {
+        std::cout << "Avaiable Options: \n";
+        for (auto opt : options)
+        {
+            std::cout << opt << std::endl;
+        }
     }
 };
