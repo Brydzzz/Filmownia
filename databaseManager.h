9635a3220e1c09b5993567237bac7b68b94736bf
bsilawko <mrufkazkrem@protonmail.com>
2024-06-04 22:39:38 +0200

    added directrs db

--- databaseManager.h
+++ databaseManager.h
@@ -1,14 +1,17 @@
 #include "csv.h"
 #include "global.h"
 #include "person.h"
-class DatabaseManager
-{
+class DatabaseManager {
     std::string moviesDb;
     std::string actorsDb;
     std::string reviewsDb;
+    std::string directorsDb;
 
-public:
-    DatabaseManager(std::string moviesDb = "../movies.csv", std::string actorsDb = "../actors.csv", std::string reviewsDb = "reviews.csv")
+   public:
+    DatabaseManager(std::string moviesDb = "../movies.csv",
+                    std::string actorsDb = "../actors.csv",
+                    std::string reviewsDb = "../reviews.csv",
+                    std::string directorsDb = "../directors.csv")
         : moviesDb(moviesDb), actorsDb(actorsDb), reviewsDb(reviewsDb){};
     std::vector<Film *> movieSearch(const std::string &title);
     std::vector<Actor> actorSearch(const std::string &name);
