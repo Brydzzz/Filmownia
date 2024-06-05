bd2385c759c6d8e95be67930ea60a0fd4e002b65
bsilawko <mrufkazkrem@protonmail.com>
2024-06-05 13:36:37 +0200

    message for when only on item was found

--- browsePage.cpp
+++ browsePage.cpp
@@ -16,35 +16,28 @@ std::unique_ptr<Page> BrowsePage::doAction(program_state act,
         std::cin.clear();
         std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
         std::getline(std::cin, title);
-        // std::cin >> title1;
-        // cppIO::input("Searched movie: ", title1, title2, title3, title4);
         DatabaseManager db_mgmt;
         std::vector<Film *> found = db_mgmt.movieSearch(title);
         if (found.size() != 0) {
             int a;
-            if (found.size() != 1) {
-                std::cout << "Found movies: " << std::endl;
-                int i = 1;
+            std::cout << "Found movies: " << std::endl;
+            int i = 0;
 
-                for (auto f : found) {
-                    if (i <= 10) {
-                        std::cout << i << '.' << f->getTitle() << std::endl;
-                        ++i;
-                    }
+            for (auto f : found) {
+                if (i < 10) {
+                    std::cout << i + 1 << '.' << f->getTitle() << std::endl;
+                    ++i;
                 }
-                cppIO::input(
-                    "Choose number of a movie you wish to see or -1 for exit: ",
-                    a);
-                if (a == -1 || a < -1 || a >= found.size()) {
-                    std::unique_ptr<BrowsePage> ptr =
-                        std::make_unique<BrowsePage>();
-                    return ptr;
-                }
-            } else {
-                a = 1;
             }
-            a--;
-            // std::cout << found[a].getDesc() << std::endl;
+            cppIO::input(
+                "Choose number of a movie you wish to see or -1 for exit: ", a);
+            if (a <= -1 || a > found.size()) {
+                std::unique_ptr<BrowsePage> ptr =
+                    std::make_unique<BrowsePage>();
+                return ptr;
+            } else {
+                a--;
+            }
             std::unique_ptr<FilmPage> ptr =
                 std::make_unique<FilmPage>(found[a]);
             return ptr;
@@ -64,28 +57,24 @@ std::unique_ptr<Page> BrowsePage::doAction(program_state act,
         std::vector<Actor> foundActors = db_mgmt.actorSearch(name);
         if (foundActors.size() != 0) {
             int a;
-            if (foundActors.size() != 1) {
-                std::cout << "Found actors: " << std::endl;
-                int i = 1;
+            std::cout << "Found actors: " << std::endl;
+            int i = 0;
 
-                for (auto a : foundActors) {
-                    if (i <= 10) {
-                        std::cout << i << '.' << a.getName() << std::endl;
-                        ++i;
-                    }
+            for (auto a : foundActors) {
+                if (i < 10) {
+                    std::cout << i + 1 << '.' << a.getName() << std::endl;
+                    ++i;
                 }
-                cppIO::input(
-                    "Choose number of a actor you wish to see or -1 for exit: ",
-                    a);
-                if (a == -1) {
-                    std::unique_ptr<BrowsePage> ptr =
-                        std::make_unique<BrowsePage>();
-                    return ptr;
-                }
-            } else {
-                a = 1;
             }
-            a--;
+            cppIO::input(
+                "Choose number of a actor you wish to see or -1 for exit: ", a);
+            if (a <= -1 || a > foundActors.size()) {
+                std::unique_ptr<BrowsePage> ptr =
+                    std::make_unique<BrowsePage>();
+                return ptr;
+            } else {
+                a--;
+            }
             std::unique_ptr<ActorPage> ptr =
                 std::make_unique<ActorPage>(foundActors[a]);
             return ptr;
@@ -105,28 +94,24 @@ std::unique_ptr<Page> BrowsePage::doAction(program_state act,
         std::vector<Director> foundDirectors = db_mgmt.directorSearch(name);
         if (foundDirectors.size() != 0) {
             int a;
-            if (foundDirectors.size() != 1) {
-                std::cout << "Found directors: " << std::endl;
-                int i = 1;
+            std::cout << "Found directors: " << std::endl;
+            int i = 0;
 
-                for (auto d : foundDirectors) {
-                    if (i <= 10) {
-                        std::cout << i << '.' << d.getName() << std::endl;
-                        ++i;
-                    }
+            for (auto d : foundDirectors) {
+                if (i < 10) {
+                    std::cout << i + 1 << '.' << d.getName() << std::endl;
+                    ++i;
                 }
-                cppIO::input(
-                    "Choose number of a actor you wish to see or -1 for exit: ",
-                    a);
-                if (a == -1) {
-                    std::unique_ptr<BrowsePage> ptr =
-                        std::make_unique<BrowsePage>();
-                    return ptr;
-                }
-            } else {
-                a = 1;
             }
-            a--;
+            cppIO::input(
+                "Choose number of a actor you wish to see or -1 for exit: ", a);
+            if (a <= -1 || a > foundDirectors.size()) {
+                std::unique_ptr<BrowsePage> ptr =
+                    std::make_unique<BrowsePage>();
+                return ptr;
+            } else {
+                a--;
+            }
             std::unique_ptr<DirectorPage> ptr =
                 std::make_unique<DirectorPage>(foundDirectors[a]);
             return ptr;
@@ -151,10 +136,6 @@ program_state BrowsePage::nextAction() {
     std::string action;
     while (std::find(options.begin(), options.end(), action) == options.end()) {
         cppIO::input("Enter desired action: ", action);
-        // for (int i = 0; i < action.size(); ++i)
-        // {
-        //     action[i] = (char)tolower(action[i]);
-        // }
     }
     if (action == "Exit") {
         return program_state::Exit;
