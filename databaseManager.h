c9aabc7e12c34428440769006773031ebaf25539
Tomasz <tom.p.smolen@gmail.com>
2024-06-07 15:22:32 +0200

    searchPerson as template function

--- databaseManager.h
+++ databaseManager.h
@@ -2,17 +2,9 @@
 #include "global.h"
 #include "person.h"
 
-enum class whichDb
-{
-    moviesDb,
-    actorsDb,
-    reviewsDb,
-    directorsDb,
-    producersDb
-};
+enum class whichDb { moviesDb, actorsDb, reviewsDb, directorsDb, producersDb };
 
-class DatabaseManager
-{
+class DatabaseManager {
     std::string moviesDb;
     std::string actorsDb;
     std::string reviewsDb;
@@ -20,7 +12,7 @@ class DatabaseManager
     std::string producersDb;
     std::string enumDbToStr(whichDb db);
 
-public:
+   public:
     DatabaseManager(std::string moviesDb = "../movies.csv",
                     std::string actorsDb = "../actors.csv",
                     std::string reviewsDb = "../reviews.csv",
@@ -36,4 +28,33 @@ public:
     std::vector<Director> directorSearch(const std::string &name);
     std::vector<Producer> producerSearch(const std::string &name);
     void replaceLine(std::string newLine, std::string oldLine, whichDb db);
+    template <typename T>
+    std::vector<T> personSearch(const std::string &name) {
+        std::vector<T> persons;
+        std::string db;
+        if constexpr (std::is_same<T, Producer>::value) {
+            db = producersDb;
+        } else if constexpr (std::is_same<T, Actor>::value) {
+            db = actorsDb;
+        } else if constexpr (std::is_same<T, Director>::value) {
+            db = directorsDb;
+        }
+        io::CSVReader<4, io::trim_chars<' '>, io::no_quote_escape<';'>> in(db);
+        in.read_header(io::ignore_missing_column, "ID", "Name", "Birthday",
+                       "Films");
+        unsigned int ID;
+        std::string Name;
+        std::string Birthday;
+        std::string Films;
+        while (in.read_row(ID, Name, Birthday, Films)) {
+            if (Name.find(name) != std::string::npos) {
+                Date BirthdayDate;
+                std::istringstream bday(Birthday);
+                bday >> BirthdayDate;
+                T p(ID, Name, BirthdayDate, Films);
+                persons.push_back(p);
+            }
+        }
+        return persons;
+    }
 };
