2048f429bf70478315335161bd54414e48b7f905
Kinga <kingal@itbs.pl>
2024-06-08 00:51:09 +0200

    we probably wont need it

--- logged.h
+++ logged.h
@@ -14,8 +14,8 @@ class Logged : public Role {
     }
     // Logged(User *us, const Logged &l_user) : Role(us),
     // reviews(l_user.reviews) {}
-    Logged(User *us, std::initializer_list<Review> revs)
-        : Role(us), reviews(revs) {}
+    // Logged(User *us, std::initializer_list<Review> revs)
+    //     : Role(us), reviews(revs) {}
     Logged(User *us, std::vector<Review> revs) : Role(us), reviews(revs) {}
     void setReviews(std::vector<Review> revs) { reviews = revs; }
     const std::vector<Review> &getReviews() const;
