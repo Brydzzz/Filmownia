35b62d463607a280f66a2efc4b39d10160d1ea7c
bsilawko <mrufkazkrem@protonmail.com>
2024-06-09 17:14:23 +0200

    implemented addJob and deleteJob option

--- writerPage.cpp
+++ writerPage.cpp
@@ -30,7 +30,7 @@ program_state WriterPage::nextAction() {
     return program_state::Exit;
 }
 std::unique_ptr<Page> WriterPage::doAction(program_state act,
-                                           std::unique_ptr<Role>& us_ptr) {
+                                           std::unique_ptr<Role> &us_ptr) {
     if (act == program_state::GoBack) {
         if (filmLink != nullptr) {
             std::unique_ptr<FilmPage> ptr =
@@ -49,10 +49,116 @@ std::unique_ptr<Page> WriterPage::doAction(program_state act,
             std::make_unique<WriterPage>(writer, filmLink);
         return ptr;
     } else if (act == program_state::AddElement) {
+        DatabaseManager db_mgmt;
+        int j = 0;
+        while (j != 1 && j != 2 && j != 3) {
+            cppIO::input(
+                "Choose job: \n1. Screenplay \n2. Story \n3. Writer \n", j);
+        }
+        std::cout << "In movie: " << std::endl;
+        std::cin.clear();
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+        std::string title;
+        std::getline(std::cin, title);
+        std::vector<Film *> foundFilms = findMovies(title);
+        Film *f = chooseMovie(foundFilms);
+        if (f == nullptr) {
+            waitForInput();
+            std::unique_ptr<WriterPage> ptr =
+                std::make_unique<WriterPage>(writer, filmLink);
+            return ptr;
+        }
+        std::ostringstream os;
+        os << writer;
+        std::string oldRecord = os.str();
+        WriterType wt = static_cast<WriterType>(j);
+        try {
+            writer.addJob(wt, *f);
+        } catch (const std::invalid_argument &e) {
+            std::cout << "Writer already has a job in this movie\n";
+            waitForInput();
+            std::unique_ptr<WriterPage> ptr =
+                std::make_unique<WriterPage>(writer, filmLink);
+            return ptr;
+        }
+        os.str("");
+        os << writer;
+        std::string newRecord = os.str();
+        db_mgmt.replaceLine(newRecord, oldRecord, whichDb::writersDb);
+        os.str("");
+        os << f;
+        std::string oldMovie = os.str();
+        // f->addWriter(writer.getName(), wtypeToString(wt));
+        try {
+            f->addWriter(writer.getName(), wtypeToString(wt));
+            os.str("");
+            os << f;
+            std::string newMovie = os.str();
+            db_mgmt.replaceLine(newMovie, oldMovie, whichDb::moviesDb);
+        } catch (const std::invalid_argument &e) {
+            std::cout << "This writer never had a job in this movie in the "
+                         "first place\n";
+            waitForInput();
+            std::unique_ptr<WriterPage> ptr =
+                std::make_unique<WriterPage>(writer, filmLink);
+            return ptr;
+        }
+        // os.str("");
+        // os << f;
+        // std::string newMovie = os.str();
+        // db_mgmt.replaceLine(newMovie, oldMovie, whichDb::moviesDb);
         std::unique_ptr<WriterPage> ptr =
             std::make_unique<WriterPage>(writer, filmLink);
         return ptr;
     } else if (act == program_state::DeleteElement) {
+        DatabaseManager db_mgmt;
+        std::string title;
+        std::cout << "Delete job from movie: " << std::endl;
+        std::cin.clear();
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+        std::getline(std::cin, title);
+        std::vector<Film *> foundFilms = writer.findFilmsByTitle(title);
+        Film *f = chooseMovie(foundFilms);
+        if (f == nullptr) {
+            waitForInput();
+            std::unique_ptr<WriterPage> ptr =
+                std::make_unique<WriterPage>(writer, filmLink);
+            return ptr;
+        }
+        std::ostringstream os;
+        os << writer;
+        std::string oldRecord = os.str();
+        try {
+            writer.deleteJob(*f);
+        } catch (const std::invalid_argument &e) {
+            std::cout << "This writer never worked onn this movie in the "
+                         "first place\n";
+            waitForInput();
+            std::unique_ptr<WriterPage> ptr =
+                std::make_unique<WriterPage>(writer, filmLink);
+            return ptr;
+        }
+        os.str("");
+        os << writer;
+        std::string newRecord = os.str();
+        db_mgmt.replaceLine(newRecord, oldRecord, whichDb::writersDb);
+        os.str("");
+        os << f;
+        std::string oldMovie = os.str();
+        try {
+            f->deleteWriter(writer.getName());
+            os.str("");
+            os << f;
+            std::string newMovie = os.str();
+            db_mgmt.replaceLine(newMovie, oldMovie, whichDb::moviesDb);
+        } catch (const std::invalid_argument &e) {
+            std::cout << "This writer never had a job in this movie in the "
+                         "first place\n";
+            waitForInput();
+            std::unique_ptr<WriterPage> ptr =
+                std::make_unique<WriterPage>(writer, filmLink);
+            return ptr;
+        }
         std::unique_ptr<WriterPage> ptr =
             std::make_unique<WriterPage>(writer, filmLink);
         return ptr;
